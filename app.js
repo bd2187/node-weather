f9efc9e233a1974eedf788568bdc5a0b8bdd7769
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-08 15:10:44 -0700

    Encode userInput via encodeURIComponent and pass it into geolocationEndpoint. Use request library to make http request to endpoint and return data if request was successful. Otherwise, return string with message

--- app.js
+++ app.js
@@ -9,4 +9,25 @@ const argv = require('yargs').options('a', {
 // Sample input: "node app.js -a 'los angeles' "
 const userInput = argv.a;
 
-console.log(userInput);
+// replace white space with "%20"
+const encodedAddress = encodeURIComponent(userInput);
+
+const geolocationEndpoint = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+request({ url: geolocationEndpoint, json: true }, function(err, res, body) {
+  /*
+    if err isn't null and no results are found, return object with data. Otherwise, return string with message
+  */
+  if (!err && body.results.length !== 0) {
+    const formattedAddress = body.results[0].formatted_address;
+    const { lat, lng } = body.results[0].geometry.location;
+
+    return {
+      formattedAddress,
+      lat,
+      lng
+    };
+  } else {
+    return 'Unable to determine location';
+  }
+});
