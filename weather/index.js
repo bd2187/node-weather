9a2177a977e47a8bc619cfb0af95d543f3572b59
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-08 18:38:34 -0700

    Add comments to fetchWeather module

--- weather/index.js
+++ weather/index.js
@@ -13,6 +13,10 @@ function fetchWeather(lat, lng, cb) {
       json: true
     },
     (err, res, body) => {
+      /*
+        if no error is present and status code is 200, run callback with null as value for
+        err variable and current temperature object as value for weather response
+      */
       if (!err && res.statusCode === 200) {
         return cb(null, body.currently);
       } else {
